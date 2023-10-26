84528ad1a2c8d9d0fbd6dd885e3aec92391395ba
otaviomalvess <48070390+otaviomalvess@users.noreply.github.com>
2023-10-26 19:23:07 -0300

    Home and Recipe pages design. (#3)
    * Imported card image.
    Changed hero home image.
    
    * Added the logo in the nav bar.
    
    * Implemented card component.
    
    * Polished the home page CSS.
    Added card component.
    
    * Polished the recipe page CSS.
    Implemented recipe image selection functionality.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,18 @@
 
 <main>
 	<nav>
-		<a href="/">home</a>
-		<a href="/recipe-read">recipe-read</a>
+		<div class="links">
+			<a href="/">home</a>
+			<a href="/recipe-read">recipe-read</a>
+		</div>
+		<h2 class="logo">Receitas</h2>
 	</nav>
 	<slot />
 </main>
 
+<style>
+	.logo {
+		text-align: center;
+		margin: 0px auto 5px;
+	}
+</style>
