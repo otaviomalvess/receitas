a6a19c5e6fc697ffd04a5bac8bbbd058c2077f42
Otavio Moura Alves <otaviomalvess@gmail.com>
2023-11-08 11:34:45 -0300

    feat: make DB requests to load recipe and available tags data.

---
+++ src/routes/erecipe/[slug]/+page.server.ts
@@ -0,0 +1,36 @@
+import type { Recipe } from '$lib/Recipe';
+import prisma from '$lib/prisma';
+import type { Load } from '@sveltejs/kit';
+
+//
+export const load: Load = async ({ params }) => {
+
+	let recipe = await prisma.recipe.findFirst({
+		where: {
+			slug: params.slug
+		},
+		select: {
+			id: true,
+			ingredients: true,
+			method: true,
+			name: true,
+			pictures: true,
+			slug: false,
+			tags: true,
+		}
+	}) as Recipe;
+
+	const tags = (await prisma.tag.findFirst({
+		where: {
+			id: "8a36e3d8-eb1d-46d7-ae44-45beed126dd1"
+		},
+		select: {
+			tags: true
+		}
+	}))?.tags;
+
+	return {
+		recipe,
+		tags,
+	};
+};
